11aab291535cdc709b40ef4aab17f39872d5cbf5
PortKicker <50001800+PortKicker@users.noreply.github.com>
2019-05-31 09:03:18 -0700

    Add files via upload

--- Black_Jack.py
+++ Black_Jack.py
@@ -32,7 +32,7 @@ class Card():
 
     def __str__(self):
 
-        print('{self.rank} of {self.suit}')
+        return self.rank + ' of ' + self.suit 
 
 class Deck():
 
@@ -46,7 +46,7 @@ class Deck():
     def __str__(self):
         deck_comp = ''
         for card in self.deck:
-            deck_comp += '\n '+Card.__str__()
+            deck_comp += '\n ' + card.__str__()
         return 'the deck has: ' + deck_comp
 
     def shuffle(self):
@@ -56,7 +56,39 @@ class Deck():
         single_card = self.deck.pop()
         return single_card
 
+class Hand():
 
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+
+    def add_card(self, card):
+        
+        self.cards.append(card)
+        self.value += values[card.rank]
+        if card.rank == 'Ace':
+            self.aces += 1
+
+    def adjust_for_ace(self):
+        while self.value > 21 and self.aces:
+            self.value -= 10
+            self.aces -= 1
+
+class Chips():
+
+    def __init__(self):
+        self.total = 100
+        self.bet = 0
+
+    def win_bet(self):
+        self.total += self.bet
+
+    def lose_bet(self):
+        self.total -= self.bet
+
+aaron = Hand()
 test_deck = Deck()
+test_deck.shuffle()
 print(test_deck)
 
